17c82e5b33446f7287140f28f8df0fc38e441755
gauss963 <gauss963@gmail.com>
2024-11-04 16:36:51 +0800

    Find issue, not dp but indexing
    Fortran uses column major!!~~ not row...

--- code/GPSLocating.f90
+++ code/GPSLocating.f90
@@ -3,11 +3,20 @@ program GPSLocating
     implicit none
 
     integer, parameter :: dp = real64
-    integer :: N, i, j, k, INFO
-    real(dp), allocatable :: v_QC1(:), v_QC2(:), v_QC3(:), v_d(:)
-    real(dp) :: v_m(3)
-    real(dp), allocatable :: m_Q(:,:), m_QT(:,:)
+    integer :: N, i, j, k, l, m, INFO
+    real(dp), allocatable :: v_QC1(:), v_QC2(:), v_QC3(:), v_d(:), v_d_FIXED(:)
+    real(dp) :: v_m(3), v_location(3)
+    real(dp), allocatable :: m_Q(:,:), m_QT(:,:), m_Q_FIXED(:,:)
+
     real(dp) :: m_QTQ(3, 3), m_QTQ_INVERSE(3, 3)
+
+    ! real, allocatable :: v_QC1(:), v_QC2(:), v_QC3(:), v_d(:), v_d_FIXED(:)
+    ! real :: v_m(3), v_location(3)
+    ! real, allocatable :: m_Q(:,:), m_QT(:,:), m_Q_FIXED(:,:)
+
+    ! real :: m_QTQ(3, 3), m_QTQ_INVERSE(3, 3)
+
+    v_location = [20, 20, 20]
 
     ! Prompt user for the number of stations
     print *, 'Enter the number of stations (N >= 4):'
@@ -16,43 +25,119 @@ program GPSLocating
         print *, 'At least 4 stations are required.'
         stop
     end if
-
-    ! Allocate arrays
     allocate(v_QC1(N), v_QC2(N), v_QC3(N), v_d(N))
 
-    ! Input station coorv_dnates and v_dstances
-    print *, 'Enter the coordinates (v_QC1, v_QC2, v_QC3) and stance v_d for each station:'
+
+    ! Input station coordinates and dstance
+    print *, 'Enter the coordinates (v_QC1, v_QC2, v_QC3) and dstance v_d for each station:'
     do i = 1, N
         print *, 'Station ', i, ':'
         read *, v_QC1(i), v_QC2(i), v_QC3(i), v_d(i)
     end do
 
-    allocate(m_Q(N, 3), m_QT(3, N))
+
+    v_d_FIXED = v_d
+    ! print *, 'd_FIXED is', v_d_FIXED
+
+
+    allocate(m_Q(N, 3), m_QT(3, N), m_Q_FIXED(N, 3))
     do j = 1, N
-        m_Q(j, 1) = v_QC1(j)
-        m_Q(j, 2) = v_QC2(j)
-        m_Q(j, 3) = v_QC3(j)
-    end do
+        m_Q(j, 1) = v_QC1(j) ! (column, row) => (1, j) -> v_QC1(j)
+        m_Q(j, 2) = v_QC2(j) ! Since FORTRAN is column-major
+        m_Q(j, 3) = v_QC3(j) ! See `https://en.wikipedia.org/wiki/Row-_and_column-major_order`
 
-    ! Print the matrix to check
-    print *, 'Matrix Q:'
+        m_Q_FIXED(j, 1) = v_QC1(j)
+        m_Q_FIXED(j, 2) = v_QC2(j)
+        m_Q_FIXED(j, 3) = v_QC3(j)
+    end do
+    ! Q FIXED initialized here. Print the matrix to check
+    print *, 'Matrix Q FIXED:'
     do k = 1, N
-        print *, m_Q(k, 1), m_Q(k, 2), m_Q(k, 3)
+        print *, m_Q_FIXED(k, 1), m_Q_FIXED(k, 2), m_Q_FIXED(k, 3)
     end do
-    
 
-    m_QT = transpose(m_Q)
-    m_QTQ = matmul(m_QT, m_Q)
-    call matrix_inverse(3, m_QTQ, m_QTQ_INVERSE, INFO)
-    v_m = matmul(m_QTQ_INVERSE, matmul(m_QT, v_d))
 
+
+    do i = 1, 1000
+        
+        ! Update matrix Q
+        do j = 1, N
+            do k = 1, 3
+                m_Q(j, k) = v_location(k) - m_Q_FIXED(j, k)
+            end do
+        end do
+
+        ! Update vector d
+        do m = 1, N
+            v_d(m) = v_d_FIXED(m)**2 - ( m_Q_FIXED(m, 1) - v_location(1) )**2 &
+                                     - ( m_Q_FIXED(m, 2) - v_location(2) )**2 &
+                                     - ( m_Q_FIXED(m, 3) - v_location(3) )**2
+            
+            v_d(m) = v_d(m) * 0.5
+        end do
+        ! print *, 'vector d = ', v_d
+        ! print *, '---------------------------------------------------------------'
+
+        ! print *, 'Matrix Q:'
+        ! do k = 1, N
+        !     print *, m_Q(k, 1), m_Q(k, 2), m_Q(k, 3)
+        ! end do
+
+
+
+        ! Solve linear system
+        m_QT = transpose(m_Q)
+        m_QTQ = matmul(m_QT, m_Q)
+        
+        call matrix_inverse(3, m_QTQ, m_QTQ_INVERSE, INFO)
+        v_m = matmul(matmul(m_QTQ_INVERSE, m_QT), v_d)
+        
+        
+        ! call MATRIXINV(m_QTQ, 3)
+        ! v_m = matmul(matmul(m_QTQ, m_QT), v_d)
+        
+        v_location = v_location + v_m
+
+        ! print *, 'vector m = ', v_m
+
+
+
+
+        ! Check if \delta x, \delta y, \delta z \leq 10e-6
+        if (abs(v_m(1)) < 10e-6 .AND. & 
+            abs(v_m(2)) < 10e-6 .AND. &
+            abs(v_m(3)) < 10e-6) then
+            print *, 'Number of iteration is', i
+            goto 42
+        end if
+
+
+    end do
+
+
+
+
+
+
+
+
+
+
+
+
+    print *, 'Exceed iteration limit (1000)'
+    print *, ''
 
     ! Output the result
-    print *, 'The estimated coordinates of the target point are:'
-    print *, 'X = ', v_m(1)
-    print *, 'Y = ', v_m(2)
-    print *, 'Z = ', v_m(3)
+42  print *, 'The estimated coordinates of the target point are:'
+    print *, 'X = ', v_location(1)
+    print *, 'Y = ', v_location(2)
+    print *, 'Z = ', v_location(3)
 
+    print *, 'The errors are:'
+    print *, 'delta x = ', v_m(1)
+    print *, 'delta y = ', v_m(2)
+    print *, 'delta z = ', v_m(3)
 
 
     deallocate(v_QC1, v_QC2, v_QC3, v_d)
